e51e166344afe41d9c967702381ca28f1f03e6af
C1998-ops <111860290+C1998-ops@users.noreply.github.com>
2023-09-11 21:28:32 +0530

    newchanes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useMemo, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = useMemo(() => {
+    return () => {
+      setCount(count + 1);
+    };
+  }, [count]);
+  const handleSubmit = () => {
+    if (count !== 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span>{count}</span>
+        <button onClick={handleClick}>adding</button>
+        <button onClick={handleSubmit}>subtraction</button>
         <a
           className="App-link"
           href="https://reactjs.org"
